787bc823c2732a794dae438cd58c04707b502bfd
Ed Tischler <etischler@ufl.edu>
2016-07-07 14:26:19 -0400

    first commit

---
+++ LoginPage.swift
@@ -0,0 +1,36 @@
+//
+//  LoginPage.swift
+//  Odds?!
+//
+//  Created by Edward Tischler on 7/7/16.
+//  Copyright © 2016 Edward Tischler. All rights reserved.
+//
+
+import UIKit
+
+class LoginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //print("hello fuckboi")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Odds?!/ObjCBridge.h
@@ -0,0 +1,16 @@
+//
+//  ObjCBridge.h
+//  Odds?!
+//
+//  Created by Edward Tischler on 7/6/16.
+//  Copyright © 2016 Edward Tischler. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* ObjCBridge_h */
