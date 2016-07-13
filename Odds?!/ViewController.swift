ca87b3a1c3d65c4cbe07c5595fba7933665d6d1e
Ed Tischler <etischler@ufl.edu>
2016-07-13 15:06:15 -0400

    aws added + pop up

--- Odds?!/ViewController.swift
+++ Odds?!/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
     @IBOutlet weak var ChallengeButtonOutlet: UIButton!
     @IBOutlet weak var PendingChallengesOutlet: UIButton!
-    
+    let loginView : FBSDKLoginButton = FBSDKLoginButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,11 +20,23 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         {
             // User is already logged in, do work such as go to next view controller.
             print("user already loggin in")
+            //GET RID OF THE FOLLOWING BLOCK FOR PRODUCTION
+            
+            loginView.loginBehavior = FBSDKLoginBehavior.Web
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+            ChallengeButtonOutlet.hidden = false
+            PendingChallengesOutlet.hidden = false
+            
+            //END OF BLOCK
         }
         else{
             //performSegueWithIdentifier("toLoginPage", sender: self)
+            print(loginView.frame.size)
+            loginView.frame.size = CGSizeMake(253.5, 45);
             
-            let loginView : FBSDKLoginButton = FBSDKLoginButton()
             loginView.loginBehavior = FBSDKLoginBehavior.Web
             self.view.addSubview(loginView)
             loginView.center = self.view.center
@@ -47,11 +59,16 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
             // Handle cancellations
             print("cancelled")
         }
-        else {
+        else {//MAKE SURE TO ADD ID****
             // If you ask for multiple permissions at once, you
             // should check if specific permissions missing
             if result.grantedPermissions.contains("email")
             {
+                ChallengeButtonOutlet.hidden = false;
+                PendingChallengesOutlet.hidden = false;
+                loginView.hidden = true;
+                
+                returnUserData()
                 // Do work
             }
         }
@@ -61,12 +78,43 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         
         print("User Logged Out")
     }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath:"me", parameters: ["fields":"email,name,id"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                //print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                //print("User Email is: \(userEmail)")
+                let userId : NSString = result.valueForKey("id") as! NSString
+                //print("User ID is: \(userId)")
+                
+                let alert = UIAlertController(title: "Welcome!", message: ("Hello " + (userName as String) + ", we at Odds noticed you are a new user and would like to say thanks for joining! Don't know how to play? Sure! Under 'Challenge a Friend', be sure to chose the bot and we will walk you through your first game! Have fun and happy daring!"), preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.Default, handler: nil))
+                self.presentViewController(alert, animated: true, completion: nil)
+                
+                
+                
+                
+            }
+        })
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+	
 
 }
 
---
+++ Pods/Target Support Files/Pods-Odds?!/Pods-Odds?!-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Odds__ : NSObject
+@end
+@implementation PodsDummy_Pods_Odds__
+@end
