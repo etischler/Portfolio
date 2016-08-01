0b181d246332ffa191cbdf79cd409c2ced3faf66
Edward R. Tischler <tischler@stormx.cise.ufl.edu>
2016-08-01 09:58:23 -0400

    add nic on network script

---
+++ add_nic.ps1
@@ -0,0 +1,25 @@
+﻿clear
+$netName = Read-Host 'Provide me the name of the network please. For example : xx.xxx.xx.x/xx'
+echo "Please wait while I query"
+echo ""
+$VMs = Get-VM | where {$_.NetworkAdapters.NetworkName -eq $netName}
+echo "These will be the VMs that receive a new nic:"
+echo $VMs.Name
+echo ""
+$proceed = Read-Host 'Proceed? (y/n)'
+if($proceed -eq 'y'){
+    echo "Proceeding..."
+    echo ""
+    foreach($VM in $VMs){
+        Get-VM $VM.Name | New-NetworkAdapter  -NetworkName $netName -StartConnected -Type VMXNET3
+        echo ""
+        echo "$VM has received a nic"
+        echo ""
+        #Read-Host 'Proceed? (y/n)'
+    }
+}
+else{
+echo "User aborted."
+}
+
+
